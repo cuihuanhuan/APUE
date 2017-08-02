0c4afa7ad12a510aa39a87db5ddb4ce870ba7772
James Wang <jnwang@suse.com>
2017-08-02 09:53:21 +0800

    Add some syscalls examples

---
+++ syscall/exit_syscall_64bit.c
@@ -0,0 +1,18 @@
+
+	int
+main(int argc, char *argv[])
+{
+	unsigned long syscall_nr = 60;
+	long exit_status = 42;
+
+	asm ("movq %0, %%rax\n"
+	"movq %1, %%rdi\n"
+	"syscall"
+	: /* output parameters, we aren't outputting anything, no none */
+	/* (none) */
+	: /* input parameters mapped to %0 and %1, repsectively */
+	"m" (syscall_nr), "m" (exit_status)
+	: /* registers that we are "clobbering", unneeded since we are calling exit */
+	"rax", "rdi");
+}
+
